dcaed0a7c88563f779e1c47caeda72514b8f9f68
BogdanAang <bogdan.9118@gmail.com>
2017-12-11 11:37:38 +0200

    initial commit

---
+++ web/index.php
@@ -0,0 +1,44 @@
+<html>
+<head>
+<title>Training Index Page</title>
+	
+	<link href="assets/css/style.css" rel="stylesheet">
+	
+</head>
+<body>
+
+<img class="prod" src="">
+<ul>
+
+	<li>Title</li>
+	<li>Description</li>
+	<a class="add" id="1" href="#">Add</a>
+	<li>Price</li>
+
+</ul>
+
+
+<ul>
+
+	<li>Title</li>
+	<li>Description</li>
+	<a class="add" id="2" href="#">Add</a>
+	<li>Price</li>
+
+</ul>
+
+
+<ul>
+
+	<li>Title</li>
+	<li>Description</li>
+	<a class="add" id="3" href="#">Add</a>
+	<li>Price</li>
+
+</ul>
+
+
+<a href="#">Go to cart</a>
+
+</body>
+</html>
